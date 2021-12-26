4bc7f51dd23a641f5907a23c15a00bf83491f4b9
Daniil <zds.irk@gmail.com>
2021-12-26 09:55:56 +0800

    update: task_5

--- task_5.swift
+++ task_5.swift
@@ -9,85 +9,85 @@ class SortDictionary {
 
         switch operatorType{
         case ">":
-            for (index, value) in dictionary.sorted(by: {$0.value < $1.value} ){
+            for (index, value) in dictionary.sorted(by: {$0.key < $1.key}){
                 if (index > number){
                     print("\(index)=\(value)")
                 }
             }
         case ">=":
-            for (index, value) in dictionary.sorted(by: {$0.value < $1.value} ){
+            for (index, value) in dictionary.sorted(by: {$0.key < $1.key}){
                 if (index >= number){
                     print("\(index)=\(value)")
                 }
             }
         case "<":
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value} ){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index < number){
                     print("\(index)=\(value)")
                 }
             }
         case "<=":
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value} ){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index <= number){
                     print("\(index)=\(value)")
                 }
             }
         default:
-            for (index, value) in dictionary.sorted(by: {$0.value < $1.value} ){
-                print("\(index)=\(value)")
-            }
+            print("Введен неверный оператор сравнения")
         }
     }
     
     func sortByKeysIn(firstOperator : String, firstNumber : Int, secondOperator : String, secondNumber : Int){
         if (firstOperator == ">" && secondOperator == "<"){
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value}).reversed(){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index > firstNumber && index < secondNumber){
                     print("\(index)=\(value)")
                 }
             }
         }else if(firstOperator == "<" && secondOperator == ">"){
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value}).reversed(){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index < firstNumber && index > secondNumber){
                     print("\(index)=\(value)")
                 }
             }
         }else if(firstOperator == "<=" && secondOperator == ">="){
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value}).reversed(){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index <= firstNumber && index >= secondNumber){
                     print("\(index)=\(value)")
                 }
             }
         }else if(firstOperator == ">=" && secondOperator == "<="){
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value}).reversed(){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index >= firstNumber && index <= secondNumber){
                     print("\(index)=\(value)")
                 }
             }
         }else if(firstOperator == ">" && secondOperator == "<="){
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value}).reversed(){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index > firstNumber && index <= secondNumber){
                     print("\(index)=\(value)")
                 }
             }
         }else if(firstOperator == ">=" && secondOperator == "<"){
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value}).reversed(){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index >= firstNumber && index < secondNumber){
                     print("\(index)=\(value)")
                 }
             }
         }else if(firstOperator == "<=" && secondOperator == ">"){
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value}).reversed(){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index <= firstNumber && index > secondNumber){
                     print("\(index)=\(value)")
                 }
             }
         }else if(firstOperator == "<" && secondOperator == ">="){
-            for (index, value) in dictionary.sorted(by: {$0.value > $1.value}).reversed(){
+            for (index, value) in dictionary.sorted(by: {$0.key > $1.key}).reversed(){
                 if (index < firstNumber && index >= secondNumber){
                     print("\(index)=\(value)")
                 }
             }
+        }else{
+            print("Введен неверный оператор сравнения")
         }
         
     }
@@ -98,7 +98,10 @@ var dict = [1 : "100", 2 : "200", 3 : "300", 4 : "400", 5 : "500", 6 : "600", 7 
 
 var myDict = SortDictionary(dictionary: dict)
 // Обычная сортировка
-myDict.sortByKeys(operatorType: ">", number: 5)
+myDict.sortByKeys(operatorType: "<=", number: 7)
 print("")
 //Сортировка по диапазону
-myDict.sortByKeysIn(firstOperator: ">", firstNumber: 5, secondOperator: "<", secondNumber: 8)
+myDict.sortByKeysIn(firstOperator: ">=", firstNumber: 8, secondOperator: "<", secondNumber: 12)
+
+
+
